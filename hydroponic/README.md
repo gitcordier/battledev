12bca65a942a42043e0417a5820de0c7f3f462a1
gcordier <root@gcordier.eu>
2019-02-28 17:02:05 +0100

    UPDATE: shorter version

---
+++ hydroponic/README.md
@@ -0,0 +1,9 @@
+# Solution to the hydroponic problem (battledev (FR) 2017/11)
+Source: 
+https://questionsacm.isograd.com/codecontest/pdf/hydropons.pdf
+https://github.com/gitcordier/battledev/blob/master/hydroponic/hydropons.pdf
+
+Name: qad.py
+Indent: 4 spaces (no tab).
+Width: 79 (comment lines < 73).
+License: Nope; consider the below code as public domain.
--- hydroponic/qad.py
+++ hydroponic/qad.py
@@ -1,76 +1,27 @@
-###############################################################################
-# Solution to the hydroponic problem (battledev (FR) 11/2017)                 #
-#     https://questionsacm.isograd.com/codecontest/pdf/hydropons.pdf          #
-# Name:     qad.py .                                                          #
-# Indent:   = 4 spaces (no tab).                                              #
-# Width:    79 (comment lines < 73).                                          #
-# license:  Nope; consider the below code as public domain.                   #
-###############################################################################
-"""
-    Row 0 : The size N = 3, 4, ..., 50 of the whole field (identified 
-        with a (N, N) square.
-    Row 1, ...,i, ..., N : Row i of the aforementioned square, as string 
-        of size N. The characters of such string are either 'X' (marking
-        a water plant) either a blank space.
-"""
-input = [ # for example... 
-    "3", 
-    "X..",
-    "...",
-    "..X",
-]
-
+#Row 0 : The size N = 3, 4, ..., 50 of the whole field (identified 
+# with a (N, N) square.
+# Row 1, ...,i, ..., N : Row i of the aforementioned square, as string 
+# of size N. The characters of such string are either 'X' (marking
+# a water plant) either a blank space.
+#
+import sys
 
 lines = []
-for line in input:
+for line in sys.stdin:
 	lines.append(line.rstrip('\n'))
 
-N               = int(lines[0])
-map_of_water    = lines[1:]
+N = int(lines[0])
+d = [[w for w in r] for r in lines[1:]]
+w = set((i, j) for j in range(N) for i in range(N) if (d[i][j] is 'X'))
+r = set()
 
-water   = set()     # The water plants.
-result  = set()     # Union of the cultivable fieds and the water plants 
-
-def create_water():
-    """
-        We enlist X/water plants positions.
-    """
-    for i in range(N):
-        string = map_of_water[i] 
-        for j in range(N):
-            if string[j] == 'X':
-                water.add((i, j))
-#
-
-def update_result(i, j):
-    if is_in_bounds(i, j):
-        result.add((i, j))
-#
-
-def is_in_bounds(i, j):
-    if i in range(N)  and j in range(N):
-        return True 
-    return False 
-#
-
-def is_there_water_in(i, j):
-    for element in water:
-        if element[0] == i and element[1] == j:
-            return True 
-    return False
-#
-
-# Veni
-create_water()
-
-# Vidi
 for i in range(N):
     for j in range(N):
-        if is_there_water_in(i, j):
+        if (i, j) in w:
             for v in range(-1, 2):
                 for h in range(-1, 2):
-                    update_result(i+v, j+h)
-#
-
-# Vici
-print(len(result) -len(water))
+                    t = (i + v, j + h)
+                    if all(x in range(N) for x in t):
+                        r.add(t)
+                    
+print(len(r) - len(w))
